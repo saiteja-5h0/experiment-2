3ae90e3120fcd21eb358936b95eed5c1dc817832
student <student@172.16.103.33>
2025-12-30 10:54:42 +0530

    exp commit

---
+++ src/main/resources/experiment2.java
@@ -0,0 +1,8 @@
+
+public class experiment2 {
+	public static void main(String are[])
+	{
+		System.out.println("welcome to new commits");
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2><%= "Hello World!" %></h2>
+<h1><%= "welcome to jsp file" %></h1>
 </body>
 </html>
